9bfaaa5e677ca3f81245de04354cae1962e1c7a2
phakin_melo99 <phakin135@gmail.com>
2025-01-02 01:29:10 +0700

    Update flux.sh

--- config/provisioning/flux.sh
+++ config/provisioning/flux.sh
@@ -60,14 +60,14 @@ function provisioning_start() {
     source /opt/ai-dock/bin/venv-set.sh comfyui
 
     # Get licensed models if HF_TOKEN set & valid
-    if provisioning_has_valid_hf_token; then
-        CHECKPOINT_MODELS+=("https://huggingface.co/datasets/John6666/flux1-backup-202411/acornIsSpinningFLUX_aisf11H8stpChinfx.safetensors")
-        VAE_MODELS+=("https://huggingface.co/black-forest-labs/FLUX.1-dev/resolve/main/ae.safetensors")
-    else
-        UNET_MODELS+=("https://huggingface.co/datasets/John6666/flux1-backup-202411/acornIsSpinningFLUX_aisf11H8stpChinfx.safetensors")
-        VAE_MODELS+=("https://huggingface.co/black-forest-labs/FLUX.1-schnell/resolve/main/ae.safetensors")
-        sed -i 's/flux1-dev\.safetensors/flux1-schnell.safetensors/g' /opt/ComfyUI/web/scripts/defaultGraph.js
-    fi
+    #if provisioning_has_valid_hf_token; then
+    #    CHECKPOINT_MODELS+=("https://huggingface.co/datasets/John6666/flux1-backup-202411/acornIsSpinningFLUX_aisf11H8stpChinfx.safetensors")
+    #    VAE_MODELS+=("https://huggingface.co/black-forest-labs/FLUX.1-dev/resolve/main/ae.safetensors")
+    #else
+    #    UNET_MODELS+=("https://huggingface.co/datasets/John6666/flux1-backup-202411/acornIsSpinningFLUX_aisf11H8stpChinfx.safetensors")
+    #    VAE_MODELS+=("https://huggingface.co/black-forest-labs/FLUX.1-schnell/resolve/main/ae.safetensors")
+    #    sed -i 's/flux1-dev\.safetensors/flux1-schnell.safetensors/g' /opt/ComfyUI/web/scripts/defaultGraph.js
+    #fi
 
     provisioning_print_header
     provisioning_get_apt_packages
